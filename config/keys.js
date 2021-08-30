127e17cf730c25af772337ef43e396280f2b66d3
isaacshih <56406965+isaacshih@users.noreply.github.com>
2021-08-30 00:03:40 -0700

    Added a Procfile

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID: "984236312708-uu9qbvntccer0f4lcmfahvbnpsqteg0m.apps.googleusercontent.com",
+        clientSecret: "D10np8ZaC-x_TFJYwMpC9Y-r"
+    },
+    session: {
+        cookieKey: 'gonnareviewabunchofreplaycodeslol'
+    },
+    admin: {
+        id: '111098674976129296089'
+    }
+}
