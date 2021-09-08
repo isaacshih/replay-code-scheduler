8789feaa25562ff71d331db5a8b656170153cf78
isaacshih <shihi@oregonstate.edu>
2021-09-08 16:06:30 -0700

    Fixed favicon error

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const favicon = require('serve-favicon')
 
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 var app = express()
+
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 var hbs = exphbs.create({
     helpers: {
